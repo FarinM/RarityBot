3ecc84c1f3547323b77fd45909de45cb362469a0
Farin M <farin681@gmail.com>
2022-05-11 20:15:36 +0200

    stuff

--- src/index.js
+++ src/index.js
@@ -1,49 +1,60 @@
-// Require the necessary discord.js classes
-import { Connection, programs } from '@metaplex/js';
-const { metadata: { Metadata } } = programs;
+import { Connection, programs } from "@metaplex/js";
+const {
+  metadata: { Metadata },
+} = programs;
 import axios from "axios";
 import { Client, Intents } from "discord.js";
-import { createRequire } from "module"; // Bring in the ability to create the 'require' method
-const require = createRequire(import.meta.url); // construct the require method
-const my_json_file = require("./config.json") //'
-var fs = require('fs');
+import { createRequire } from "module"; // 
+const require = createRequire(import.meta.url); 
+const my_json_file = require("./config.json"); 
+var fs = require("fs");
 
-
-// Create a new client instance
 const client = new Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 const map1 = new Map();
 
-// When the client is ready, run this code (only once)
 client.once("ready", () => {
-    map1.set('a', 1);
   console.log("Ready!");
-  console.log("add")
 });
 
-
-client.on('messageCreate', async message => {
-    console.log("!rarity");
+client.on("messageCreate", async (message) => {
   if (message.content.startsWith("!rarity")) {
-    let con = new Connection('mainnet-beta');
+    let con = new Connection("http://185.191.127.226:8899/");
 
-    const metadata1 = await Metadata.load(con, await Metadata.getPDA("5bvfccqM5i9K8onJffUxUnDZoabcG8fGwpURwELtYB9F"));
+    const metadata1 = await Metadata.load(
+      con,
+      await Metadata.getPDA("A53por1wdeeaTUhrHpk6fjiuumDi46e7UwnDRVd5CtwT")
+    );
     let a = await axios.get(metadata1.data.data.uri);
-    console.log(a.data)
-    let jsn = JSON.stringify(a.data)
+    let jsn = JSON.stringify(a.data.attributes);
 
+    for (var key in a.data.attributes) {
+      let s = `${JSON.stringify(
+        a.data.attributes[key].trait_type
+      )}_${JSON.stringify(a.data.attributes[key].value)}`.replaceAll('"', "");
 
+      if(map1.get(s) == null){
+        map1.set(s, 1)
+      } else {
+        map1.set(s, map1.get(s) +1)
+      }
+      console.log(s);
+    }
 
-    fs.writeFile("thing.json", jsn, function(err, result) {
-        if(err) console.log('error', err);
+    fs.writeFile("thing.json", jsn, function (err, result) {
+      if (err) console.log("error", err);
     });
 
     message.reply({
-        files: ['./thing.json']
-    })
+      files: ["./thing.json"],
+    });
+  }
+
+  if (message.content.startsWith("!map")) {
+    console.log(map1)
   }
 });
 
